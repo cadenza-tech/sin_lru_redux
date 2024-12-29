20f0f7eb7e44f3e36c70259727babcbc76458cc2
Masahiro <watanabe@cadenza-tech.com>
2024-12-29 23:29:43 +0900

    Improve tests

--- Rakefile
+++ Rakefile
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 require 'bundler/gem_tasks'
-require 'rake/testtask'
+require 'minitest/test_task'
 require 'rubocop/rake_task'
 
-Rake::TestTask.new(:test) do |t|
-  t.pattern = 'test/**/*_test.rb'
-end
+Minitest::TestTask.create
 
 RuboCop::RakeTask.new
---
+++ test/ttl/test_thread_safe_cache.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require_relative 'test_cache'
+
+module Ttl
+  class TestThreadSafeCache < Ttl::TestCache
+    def setup
+      Timecop.freeze(Time.now)
+      @c = LruRedux::TTL::ThreadSafeCache.new(3, 5 * 60)
+    end
+
+    def test_recursion
+      @c[:a] = 1
+      @c[:b] = 2
+
+      # should not blow up
+      @c.each do |k, _| # rubocop:disable Style/HashEachMethods
+        @c[k]
+      end
+    end
+  end
+end
